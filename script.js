1bd241dbd7c374d31a145bf16ad1f5fa3908839f
OM RAJPAL <92999504+omrajpal13274@users.noreply.github.com>
2024-02-23 00:26:27 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -8,40 +8,82 @@ var confessions = [
     good: "Good: Andhere me dikhta nai",
     bad: "Bad: GenSec",
     funny: "Funny: Pata nai",
+    name: "Parag",
   },
   {
-    good: "Good: Smiles everytime",
+    good: "Good: Muslim hai",
     bad: "Bad: Dusra ka code erase krke apna khud ka likhna",
     funny: "Funny: Punjabi pasand hai isko",
+    name: "Zubair",
   },
   {
     good: "Good: Cheeks",
     bad: "Bad: Mohin",
     funny: "Funny: Mota hota tha",
+    name: "Madhav",
   },
   {
-    good: "Good: Muslim hai",
+    good: "Good: Smiles everytime",
     bad: "Bad: Doesn't talk too much",
     funny: "Funny: He is roommate of tanveer pardhaan",
+    name: "Reetinder",
   },
   {
     good: "Good: knowledge about tech like web dev, ai, ml, good cg",
     bad: "Bad: Excessive Study",
     funny: "Funny: Called Pi",
+    name: "Arshdeep",
+  },
+
+  {
+    good: "Good: Wheely bohit achi marta he",
+    bad: "Bad: Kisi ka bhai aur kisi ka bhai",
+    funny: "Funny: Juniors se iron karata he",
+    name: "Salin",
+  },
+
+  {
+    good: "Good: Patience",
+    bad: "Bad: Ram mandir ki story nhi lgai thi",
+    funny: "Funny: ********o ki collection dekhne ka shonk h",
+    name: "Zubair",
+  },
+
+  {
+    good: "Good: Boht sare charche he iske bare me",
+    bad: "Bad: Bal thore kam he 🙃",
+    funny: "Funny: Brand ambassador of brave browser",
+    name: "Raj",
+  },
+  {
+    good: "Good: Nimble witted",
+    bad: "Bad: Too much obedient with all",
+    funny: "Funny: []",
+    name: "Rachan",
+  },
+
+  {
+    good: "Good: Too good at whatever he/she does",
+    bad: "Bad: Too good at whatever he/she does",
+    funny:
+      "Funny: I had to write about her even though I didn't even know her😅",
+    name: "Anubha",
   },
 ];
-var names = ["Parag", "Reetinder", "Madhav", "Zubair", "Arshpreet"];
 var index = 0;
 function updatecount() {
   display.textContent = time;
 }
 
 function displaymessage() {
-  message.textContent = names[index];
+  message.textContent = confessions[index].name;
   message.style.display = "block";
 }
 
 function countdown() {
+  confess_good.textContent = confessions[index].good;
+  confess_bad.textContent = confessions[index].bad;
+  confess_funny.textContent = confessions[index].funny;
   var count = setInterval(() => {
     time--;
     updatecount();
@@ -55,14 +97,12 @@ function countdown() {
       display.style.color = "yellow";
     } else if (time < 10) {
       display.style.color = "red";
+    } else {
+      display.style.color = "green";
     }
-    else{
-        display.style.color = "green"
-    }
-  }, 1000);
+  }, 100);
 }
 
-countdown();
 
 var animationData = {
   container: document.getElementById("lottie"),
@@ -73,22 +113,22 @@ var animationData = {
 };
 
 var animdata = {
-    container: document.getElementById("lottie_time"),
-    renderer: "svg",
-    loop: true,
-    autoplay: true,
-    path: "/assets/timer.json"
-}
+  container: document.getElementById("lottie_time"),
+  renderer: "svg",
+  loop: true,
+  autoplay: true,
+  path: "/assets/timer.json",
+};
 var splash_anim = {
-    container: document.getElementById("splash_lottie"),
-    renderer: "svg",
-    loop: true,
-    autoplay: true,
-    path: "/assets/loading.json"
-}
+  container: document.getElementById("splash_lottie"),
+  renderer: "svg",
+  loop: true,
+  autoplay: true,
+  path: "/assets/loading.json",
+};
 
-lottie.loadAnimation(animdata)
-lottie.loadAnimation(splash_anim)
+lottie.loadAnimation(animdata);
+lottie.loadAnimation(splash_anim);
 
 var play = document.getElementById("play");
 
@@ -98,6 +138,7 @@ play.addEventListener("click", () => {
     top: targetY,
     behavior: "smooth",
   });
+  countdown();
 });
 
 var next = document.getElementById("next");
@@ -112,25 +153,20 @@ next.addEventListener("click", () => {
 
   index = (index + 1) % confessions.length;
   if (time === 0) {
-    console.log("count is 0");
-    console.log("clicked");
-
     countdown();
     time = 61;
     message.style.display = "none";
   } else {
-    console.log("clicked");
     time = 61;
   }
 });
 
-document.addEventListener("DOMContentLoaded", function() {
-    setTimeout(function() {
-        var splash = document.getElementById('splash');
-        var page = document.getElementById('main');
+document.addEventListener("DOMContentLoaded", function () {
+  setTimeout(function () {
+    var splash = document.getElementById("splash");
+    var page = document.getElementById("main");
 
-        splash.style.display = 'none';
-        page.style.display = 'block';
-    }, 3000);
+    splash.style.display = "none";
+    page.style.display = "block";
+  }, 3000);
 });
-
